c0a4b0e1032c27566d9d0fbeb457c7a2df4cbd1a
xandeletro <xandeletroalves@gmail.com>
2019-08-14 00:48:06 -0300

    helloadd

---
+++ hello.py
@@ -0,0 +1,15 @@
+from flask import Flask				# importa o flask
+
+app = Flask(__name__)				# Inicia o Flask
+
+# rotas basicas 
+
+@app.route('/hello')						# atribui uma rota ao hello_world
+def hello_world():
+	return 'Hello World!!'
+
+@app.route('/hi')						# atribui uma rota ao hi_world
+def hi_world():
+	return 'hi World!!'
+
+app.run()							# roda a aplicacao
